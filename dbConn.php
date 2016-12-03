54091541dc29b22713049cbfdca4d64afe2560e3
bilalsattar24 <bilalsattar24@gmail.com>
2016-12-03 14:54:50 -0800

    dbconn.php

---
+++ dbConn.php
@@ -0,0 +1,30 @@
+<?php
+function getDatabaseConnection(){
+    $servername = getenv('IP');
+    $dbPort = 3306;
+    
+    // Which database (the name of the database in phpMyAdmin)?
+    $database = "finalProject";
+    
+    
+    // My user information...I could have prompted for password, as well, or stored in the
+    // environment, or, or, or (all in the name of better security)
+    $username = getenv('C9_USER');
+    $password = "test";
+    
+    // Create connection
+    $conn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
+    // Check connection
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    return $conn;
+}
+
+function getDataBySQL($sql) {
+    global $connection;
+    $statment = $connection -> prepare($sql);
+    $statment -> execute();
+    $records = $statment -> fetchALL (PDO::FETCH_ASSOC);
+    return $records;
+}
+
+?>
