5b3369378eeb88659b9ac8327c6192a631f586a9
bilalsattar24 <bilalsattar24@gmail.com>
2016-12-03 15:47:09 -0800

    edit dbconn

--- dbConn.php
+++ dbConn.php
@@ -4,13 +4,13 @@ function getDatabaseConnection(){
     $dbPort = 3306;
     
     // Which database (the name of the database in phpMyAdmin)?
-    $database = "finalProject";
+    $database = "dori1240";
     
     
     // My user information...I could have prompted for password, as well, or stored in the
     // environment, or, or, or (all in the name of better security)
-    $username = getenv('C9_USER');
-    $password = "test";
+    $username = "dori1240";
+    $password = "4d6d4c2fd5b3022";
     
     // Create connection
     $conn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
