c349941e9b3aac156328e5141f62be27d3c21a64
bilalsattar24 <bilalsattar24@gmail.com>
2016-12-03 18:16:46 -0800

    new reports.php

--- reports.php
+++ reports.php
@@ -3,17 +3,45 @@
     $connection = getDatabaseConnection();
     session_start();
     
-    function avgPrice() {
+    function listPlayers() {
         $sql = "SELECT * FROM IM_teams";
         $teams = getDataBySQL($sql);
        
         foreach($teams as $team) {
             echo $team['teamName'];
         }
-        }
+        
+    }
     
+    function insertStats() {
+       
+        $sql =
+        "insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (2, 1, 2, 4, 2, 2, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (3, 1, 1, 2, 3, 3, 0);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (4, 1, 2, 5, 0, 1, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (5, 1, 1, 1, 0, 3, 1);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (6, 1, 1, 7, 1, 2, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (7, 1, 1, 6, 1, 0, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (8, 1, 2, 7, 2, 0, 1);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (9, 1, 2, 5, 1, 0, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (10, 1, 2, 2, 2, 2, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (11, 1, 1, 2, 0, 2, 2);
+                insert into IM_stats (playerID, gameID, teamID, points, rebounds, assists, fouls) values (12, 1, 1, 0, 3, 1, 1);
+            ";
+        
+         $statement = $connection->prepare($sql);
+             $statement->execute($namedParameters);
+        
+    }
     
-   
+    function insertGameLog(){
+        $sql =
+        "insert into IM_games (gameID, team1_ID, team2_ID, team1_score, team2_score, winner) values (1, 2, 0, 1, 2, 1);";
+        
+         $statement = $connection->prepare($sql);
+             $statement->execute($namedParameters);
+    }
+    
     function maxPrice() {
         $sql = "SELECT * from cars";
         $records = getDataBySQL($sql);
